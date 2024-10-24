4ccf5bae383480077d1ac14f5489141b8d13e2ee
NareshRoyal1 <reachmeatnareshroyal@gmail.com>
2024-10-24 19:20:56 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+package javaBasics;
+
+public class HelloWorld {
+	public static void main(String args[]) {
+		boolean h=true;
+		int a=2147483647;//-2,147,483,648 to 2,147,483,647
+		char b='c';// single quotes
+		byte c= 127;//-128 to 127
+		short d=32767;//-32,768 to 32,767
+		long e=987654356789876L;//L mandatory at the end
+		float f=1.2345617f;//f is mandatory at the end, upto 7 decimals
+		double g=1.98765432104329865d;// d is mandatory at the end, upto 17 decimals
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+	}
+
+}
